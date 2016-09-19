19429ce2d689060f3bd947d035ac1cd17b81b35f
Douglas Lima <douglasproglima@gmail.com>
2016-09-19 15:07:51 -0300

    06 - Arquivos Estáticos

---
+++ admin.php
@@ -0,0 +1,40 @@
+<!DOCTYPE >
+<html lang="pt-br" xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta charset="utf-8" />
+		<title>Administração</title>
+		<meta name="description" content="" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css" />
+		<link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+	</head>
+	<body>
+		<div class="container-fluid">
+			<?php include 'includes/header.php'; ?>
+			
+			<div class="col-md-12">
+				<div class="row">
+					<div class="col-md-4">
+					
+					</div>
+					
+					<div class="col-md-4">
+						<canvas id="horarios" class="chart"></canvas>
+					</div>
+					
+					<div class="col-md-4">
+					
+					</div>
+					
+				</div>
+			</div>
+			
+		</div>
+
+		<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+		<script type="text/javascript" src="assets/js/Chart.min.js"></script>		
+		<script type="text/javascript" src="assets/js/chart.init.js"></script>
+
+	</body>
+</html>
---
+++ sobre.php
@@ -0,0 +1,30 @@
+<?php 
+include 'classe/Trafico.php';
+new Trafico();
+?>
+
+<!DOCTYPE >
+<html lang="pt-br" xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta charset="utf-8" />
+		<title>Sobre</title>
+		<meta name="description" content="" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+	</head>
+	<body>
+		<div class="container-fluid">
+			<?php include 'includes/header.php'; ?>
+			<div class="row">
+				<div class="col-md-4"></div>
+				<div class="col-md-4 top text-center">
+					<h1>Página Sobre</h1>
+				</div>
+				<div class="col-md-4"></div>
+			</div>
+		</div>
+		
+		<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+	</body>
+</html>
