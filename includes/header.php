ff3958e8fee9733087a483de27d492f2cbccbab8
Douglas Lima <douglasproglima@gmail.com>
2016-09-19 15:07:18 -0300

    05 - Páginas Home, Serviços, Contato, Sobre e Admin

---
+++ includes/header.php
@@ -0,0 +1,17 @@
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="index.php" >Google Charts</a>
+			</div>
+			<div>
+				<ul class="nav navbar-nav">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="servicos.php">Serviços</a></li>
+					<li><a href="contato.php">Contato</a></li>
+					<li><a href="sobre.php">Sobre</a></li>
+					<li><a href="admin.php">Admin</a></li>
+					<li><a href="">Logout</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
